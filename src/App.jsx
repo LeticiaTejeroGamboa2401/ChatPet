89b8743da9ea73776eee298e18fc72930ae2c8bb
Armando Tintore <jorgecanche2707@gmail.com>
2025-05-01 16:58:59 -0600

    dependencias-bootstrap-icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Login from './components/Login'
 import Home from './components/Home'
+import Dashboard from './components/Dashboard'
 
 function App() {
 
@@ -14,6 +15,9 @@ function App() {
         <Route
         path="/login"
         element={<Login/>}/>
+        <Route
+        path="/dashboard"
+        element={<Dashboard/>}/>
       </Routes>
     </>
   )
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import { Container } from '@mui/material';
+import * as React from 'react';
+
+
+export default function Dashboard(){
+    return (
+        <Container sx={{bgcolor: 'black', minHeight: "95vh"}}>
+            <h1>Hola mundo</h1>
+        </Container>
+    )
+}
