afd3560a5319be0fa2ee022c5ecd39063e96e200
Armando Tintore <jorgecanche2707@gmail.com>
2025-05-02 00:34:16 -0600

    base del dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,148 @@
-import { Container } from '@mui/material';
 import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { createTheme } from '@mui/material/styles';
+import HomeIcon from '@mui/icons-material/Home';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import { useDemoRouter } from '@toolpad/core/internal';
 
+const NAVIGATION = [
+  {
+    segment: 'inicio',
+    title: 'Inicio',
+    icon: <HomeIcon />,
+  },
+  {
+    segment: 'informacion-servicio',
+    title: 'Información del servicio',
+    icon: <HomeIcon />,
+  },
+  {
+    segment: 'reservas',
+    title: 'Reservas',
+    icon: <HomeIcon />,
+  },
+  {
+    segment: 'chats',
+    title: 'Chats',
+    icon: <HomeIcon />,
+  },
+  {
+    segment: 'historia-servicio',
+    title: 'Historial del servicio',
+    icon: <HomeIcon />,
+  },
+  {
+    segment: 'pagos-facturacion',
+    title: 'Pagos y facturación',
+    icon: <HomeIcon />,
+  },
+  {
+    segment: 'reseñas-opiniones',
+    title: 'Reseñas y opiniones',
+    icon: <HomeIcon />,
+  },
+];
 
-export default function Dashboard(){
-    return (
-        <Container sx={{bgcolor: 'black', minHeight: "95vh"}}>
-            <h1>Hola mundo</h1>
-        </Container>
-    )
+const demoTheme = createTheme({
+  cssVariables: {
+    colorSchemeSelector: 'data-toolpad-color-scheme',
+  },
+  colorSchemes: { light: true, dark: true },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 600,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+function DemoPageContent({ pathname }) {
+  return (
+    <Box
+      sx={{
+        py: 4,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
+      }}
+    >
+      <Typography>Dashboard content for {pathname}</Typography>
+    </Box>
+  );
 }
+
+DemoPageContent.propTypes = {
+  pathname: PropTypes.string.isRequired,
+};
+
+
+
+
+function Dashboard(props) {
+    const { window } = props;
+
+    const [session, setSession] = React.useState({
+      user: {
+        name: 'Bharat Kashyap',
+        email: 'bharatkashyap@outlook.com',
+        image: 'https://avatars.githubusercontent.com/u/19550456',
+      },
+    });
+
+    const authentication = React.useMemo(() => {
+      return {
+        signIn: () => {
+          setSession({
+            user: {
+              name: 'Bharat Kashyap',
+              email: 'bharatkashyap@outlook.com',
+              image: 'https://avatars.githubusercontent.com/u/19550456',
+            },
+          });
+        },
+        signOut: () => {
+          setSession(null);
+        },
+      };
+    }, []);
+
+    const router = useDemoRouter('/dashboard');
+
+    const demoWindow = window !== undefined ? window() : undefined;
+
+    return (
+      <AppProvider
+        session={session}
+        authentication={authentication}
+        navigation={NAVIGATION}
+        router={router}
+        theme={demoTheme}
+        window={demoWindow}
+      >
+        <DashboardLayout
+        slots={{
+
+        }}
+        >
+          <DemoPageContent pathname={router.pathname} />
+        </DashboardLayout>
+      </AppProvider>
+    );
+  }
+
+Dashboard.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * Remove this when copying and pasting into your project.
+   */
+  window: PropTypes.func,
+};
+
+export default Dashboard;
