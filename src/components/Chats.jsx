9ca6228d3e1744c20c597dd78ee128526f44ef20
Armando Tintore <jorgecanche2707@gmail.com>
2025-05-08 02:15:29 -0600

    Base Grid Componentes

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Typography } from '@mui/material';
 
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: (theme.vars ?? theme).palette.text.secondary,
+    ...theme.applyStyles('dark', {
+      backgroundColor: '#1A2027',
+    }),
+  }));
 
 export function Chats(){
-
+    return (
+        <Box sx={{flexGrow: 1, margin: '5vh'}}>
+            <Typography
+            sx={{mb: 2}}
+            >
+                Chats
+            </Typography>
+            <Grid container spacing={{xs: 2, md: 4}} columns={{ xs: 4, sm: 8, md: 12}}>
+                <Grid size={12}>
+                    <Item></Item>
+                </Grid>
+                <Grid size={8}>
+                    <Item></Item>
+                </Grid>
+                <Grid size={4}>
+                    <Item></Item>
+                </Grid>
+            </Grid>
+        </Box>
+    )
 }
 
 export default Chats
